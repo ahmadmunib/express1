5dd2d6a6fa5253714fd91ff0c9bfc0f985232f6a
Ahmad Munib <94528926+ahmadmunib@users.noreply.github.com>
2024-06-28 21:52:53 +0400

    to make it accept changes from the postman as well

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,30 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const socket = io();
-  
-    socket.on('update', (newArray) => {
-      const ul = document.querySelector('ul');
-      ul.innerHTML = '';
-      newArray.forEach(item => {
-        const li = document.createElement('li');
-        li.textContent = item;
-        ul.appendChild(li);
-      });
+document.addEventListener("DOMContentLoaded", () => {
+  const socket = io();
+
+  socket.on("update", (newArray) => {
+    const ul = document.querySelector("ul");
+    ul.innerHTML = "";
+    newArray.forEach((item) => {
+      const li = document.createElement("li");
+      li.textContent = item;
+      ul.appendChild(li);
     });
-  
-    const updateButton = document.getElementById('update-button');
-    if (updateButton) {
-        alert("updateButton");
-      updateButton.addEventListener('click', () => {
-        fetch('/about', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ newArray: [6, 7, 44 ,8] }),
-        })
-        .then(response => response.text())
-        .then(data => console.log(data));
-      });
-    }
   });
+
+  const updateButton = document.getElementById("update-button");
   
+  updateButton.addEventListener("click", () => {
+    fetch("/about", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // body: JSON.stringify({ newArray: [6, 7, 44 ,8] }),
+      // make a random numbers in the array (5 items only)
+      body: JSON.stringify({
+        newArray: Array.from({ length: 5 }, () =>
+          Math.floor(Math.random() * 100)
+        ),
+      }),
+    })
+      .then((response) => response.text())
+      .then((data) => console.log(data));
+  });
+});
